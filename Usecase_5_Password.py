7c21a4df5189a656a2978313e460c19d255a6d31
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-08 18:24:05 -0800

    Added: program to perform unittest on Password with minimum 8 characters

---
+++ Usecase_5_Password.py
@@ -0,0 +1,29 @@
+import unittest
+import re
+
+def validate(password):
+
+    pattern ="^[a-z0-9A-Z]{8,}$"
+    match= re.match(pattern,password)
+    if match:
+        return True
+    else:
+        return False
+
+class UserRegistration(unittest.TestCase):
+   
+    def setUp(self):
+        self.password=input("\nEnter Password : ")
+
+
+    def test_Password(self):
+        expect=True
+       
+        #Act
+        result=validate(self.password)
+        #Assert
+        self.assertEqual(result,expect,"Not Valid")
+   
+    
+if __name__=="__main__":
+    unittest.main()
